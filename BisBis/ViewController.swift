9cc70b77f97e171c4b215316d260af72975294af
Yakup AD <Yakup.Ad@d-teknoloji.com.tr>
2019-03-02 22:52:21 +0300

    Initial Commit

---
+++ BisBis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BisBis
+//
+//  Created by Yakup Ad on 2.03.2019.
+//  Copyright © 2019 Yakup Ad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
